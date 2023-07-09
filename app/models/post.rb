76e8861c4598a3fc6d1739c08e439b2a4fe94139
Miles Mosweu <tmtmosweu@gmail.com>
2023-07-09 17:23:16 +0200

    Set up post associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+  default_scope -> { order(created_at: :desc) }
 end
