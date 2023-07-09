f807943c73c31db347717e35ad3d1cf7ac3d5505
Miles Mosweu <tmtmosweu@gmail.com>
2023-07-09 17:39:53 +0200

    Add update_posts_counter method to Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,17 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
+  after_save :update_posts_counter
+
   default_scope -> { order(created_at: :desc) }
 
   def five_recent_comments
     comments.limit(5)
+  end
+
+  private
+
+  def update_posts_counter
+    author.update(posts_counter: author.posts.count)
   end
 end
