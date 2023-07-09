db8924af3d37603820dcd5de386d32a216efcdda
Miles Mosweu <tmtmosweu@gmail.com>
2023-07-09 19:22:04 +0200

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: :author_id, dependent: :destroy
   has_many :likes, foreign_key: :author_id, dependent: :destroy
 
+  validates :name, :photo, :bio, :posts_counter, presence: true
+  validates :name, :photo, :bio, string: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def three_recent_posts
     posts.limit(3)
   end
