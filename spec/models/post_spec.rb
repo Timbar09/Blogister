493990bbb3bd76a422741a4f41f06a24bf4d36a0
Miles Mosweu <tmtmosweu@gmail.com>
2023-07-10 09:20:40 +0200

    Add post model specs

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,70 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  describe "creation" do
+    before :each do
+      @austin = User.create(name: "Stone Cold", photo: "https://picsum.photos/austin.jpg", bio: "I am the rattlesnake")
+
+      @post = Post.create(title: "The toughest SOB", text: "I am the toughest SOB in the WWE, that's the bottom line, cause Stone Cold said so!", author: @austin)
+
+      @comment1 = Comment.create(text: "I was out with a couple of my buddies.", author: @austin, post: @post)
+      @comment2 = Comment.create(text: "We drank some beer.", author: @austin, post: @post)
+      @comment3 = Comment.create(text: "We drank some more beer.", author: @austin, post: @post)
+      @comment4 = Comment.create(text: "And some more beer.", author: @austin, post: @post)
+      @comment5 = Comment.create(text: "We drank all the beer.", author: @austin, post: @post)
+      @comment6 = Comment.create(text: "And then we raised some hell.", author: @austin, post: @post)
+    end
+
+    it "title can't be blank" do
+      @post.title = nil
+      expect(@post).to_not be_valid
+    end
+
+    it "text can't be blank" do
+      @post.text = nil
+      expect(@post).to_not be_valid
+    end
+
+    it "title must be at least 1 character and at most 250 characters" do
+      @post.title = ""
+      expect(@post).to_not be_valid
+      @post.title = "a" * 251
+      expect(@post).to_not be_valid
+    end
+
+    it "text must be at least 1 character and at most 500 characters" do
+      @post.text = ""
+      expect(@post).to_not be_valid
+      @post.text = "a" * 501
+      expect(@post).to_not be_valid
+    end
+
+    it "likes_counter must be an integer" do
+      @post.likes_counter = "a"
+      expect(@post).to_not be_valid
+    end
+
+    it "likes_counter must be greater than or equal to 0" do
+      @post.likes_counter = -1
+      expect(@post).to_not be_valid
+    end
+
+    it "comments_counter must be an integer" do
+      @post.comments_counter = "a"
+      expect(@post).to_not be_valid
+    end
+
+    it "comments_counter must be greater than or equal to 0" do
+      @post.comments_counter = -1
+      expect(@post).to_not be_valid
+    end
+
+    it "five_recent_comments must return 5 most recent comments" do
+      expect(@post.five_recent_comments).to eq([@comment6, @comment5, @comment4, @comment3, @comment2])
+    end
+
+    it "update_post_counter must update the posts_counter of the author" do
+      expect(@austin.posts_counter).to eq(1)
+    end
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,32 +3,41 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "creation" do
     before :each do
-      @user = User.create(name: "John Doe", photo: "https://picsum.photos/200/300", bio: "I am John Doe")
+      @cena = User.create(name: "John Doe", photo: "https://picsum.photos/200/300", bio: "I am John Doe")
+
+      @post1 = Post.create(title: "WWE Champion", text: "The champ is hereeee! Welcome to the new Raw. Raw is Cena!", author: @cena)
+      @post2 = Post.create(title: "To all challengers", text: "If you want some, come get some!", author: @cena)
+      @post3 = Post.create(title: "The new WWE title", text: "The new WWE title is here. It is the best title ever!", author: @cena)
+      @post4 = Post.create(title: "To all my fans", text: "I am sorry for not being around for a while. I was busy with my new movie. I will be back soon.", author: @cena)
     end
 
     it "name can't be blank" do
-      @user.name = nil
-      expect(@user).to_not be_valid
+      @cena.name = nil
+      expect(@cena).to_not be_valid
     end
 
     it "photo can't be blank" do
-      @user.photo = nil
-      expect(@user).to_not be_valid
+      @cena.photo = nil
+      expect(@cena).to_not be_valid
     end
 
     it "bio can't be blank" do
-      @user.bio = nil
-      expect(@user).to_not be_valid
+      @cena.bio = nil
+      expect(@cena).to_not be_valid
     end
 
     it "posts_counter must be an integer" do
-      @user.posts_counter = "a"
-      expect(@user).to_not be_valid
+      @cena.posts_counter = "a"
+      expect(@cena).to_not be_valid
     end
 
     it "posts_counter must be greater than or equal to 0" do
-      @user.posts_counter = -1
-      expect(@user).to_not be_valid
+      @cena.posts_counter = -1
+      expect(@cena).to_not be_valid
+    end
+
+    it "three_recent_posts must return 3 most recent posts" do
+      expect(@cena.three_recent_posts).to eq([@post4, @post3, @post2])
     end
   end
 end
