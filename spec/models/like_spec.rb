d74b1e56fa0910275277c6444c91db0aad445ba0
Miles Mosweu <tmtmosweu@gmail.com>
2023-07-10 10:43:15 +0200

    Resolve linters

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,12 +1,14 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  describe "creation" do
+  describe 'creation' do
     before :each do
-      @rock = User.create(name: "The Rock", photo: "https://picsum.photos/rock.jpg", bio: "If you smell what The Rock is cooking")
-      @austin = User.create(name: "Stone Cold", photo: "https://picsum.photos/austin.jpg", bio: "I am the rattlesnake")
+      @rock = User.create(name: 'The Rock', photo: 'https://picsum.photos/rock.jpg',
+                          bio: 'If you smell what The Rock is cooking')
+      @austin = User.create(name: 'Stone Cold', photo: 'https://picsum.photos/austin.jpg', bio: 'I am the rattlesnake')
 
-      @post = Post.create(title: "The People's Champ", text: "If you're not down with The Rock, I got two words for ya: SUCK IT!", author: @rock)
+      @post = Post.create(title: "The People's Champ",
+                          text: "If you're not down with The Rock, I got two words for ya: SUCK IT!", author: @rock)
 
       @austin_like = Like.create(author: @austin, post: @post)
       @rock_like = Like.create(author: @rock, post: @post)
@@ -22,7 +24,7 @@ RSpec.describe Like, type: :model do
       expect(@austin_like).to_not be_valid
     end
 
-    it "update_likes_counter must update the likes_counter of the post" do
+    it 'update_likes_counter must update the likes_counter of the post' do
       expect(@post.likes_counter).to eq(2)
     end
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "creation" do
+  describe 'creation' do
     before :each do
-      @cena = User.create(name: "John Doe", photo: "https://picsum.photos/200/300", bio: "I am John Doe")
+      @cena = User.create(name: 'John Doe', photo: 'https://picsum.photos/200/300', bio: 'I am John Doe')
 
-      @post1 = Post.create(title: "WWE Champion", text: "The champ is hereeee! Welcome to the new Raw. Raw is Cena!", author: @cena)
-      @post2 = Post.create(title: "To all challengers", text: "If you want some, come get some!", author: @cena)
-      @post3 = Post.create(title: "The new WWE title", text: "The new WWE title is here. It is the best title ever!", author: @cena)
-      @post4 = Post.create(title: "To all my fans", text: "I am sorry for not being around for a while. I was busy with my new movie. I will be back soon.", author: @cena)
+      @post1 = Post.create(title: 'WWE Champion', text: 'The champ is hereeee! Welcome to the new Raw. Raw is Cena!',
+                           author: @cena)
+      @post2 = Post.create(title: 'To all challengers', text: 'If you want some, come get some!', author: @cena)
+      @post3 = Post.create(title: 'The new WWE title', text: 'The new WWE title is here. It is the best title ever!',
+                           author: @cena)
+      @post4 = Post.create(title: 'To all my fans', text: 'Thank you for all your support!', author: @cena)
     end
 
     it "name can't be blank" do
@@ -26,17 +28,17 @@ RSpec.describe User, type: :model do
       expect(@cena).to_not be_valid
     end
 
-    it "posts_counter must be an integer" do
-      @cena.posts_counter = "a"
+    it 'posts_counter must be an integer' do
+      @cena.posts_counter = 'a'
       expect(@cena).to_not be_valid
     end
 
-    it "posts_counter must be greater than or equal to 0" do
+    it 'posts_counter must be greater than or equal to 0' do
       @cena.posts_counter = -1
       expect(@cena).to_not be_valid
     end
 
-    it "three_recent_posts must return 3 most recent posts" do
+    it 'three_recent_posts must return 3 most recent posts' do
       expect(@cena.three_recent_posts).to eq([@post4, @post3, @post2])
     end
   end
