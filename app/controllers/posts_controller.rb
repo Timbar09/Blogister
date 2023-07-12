9883f8b57284719cd7dea54bab1b2034a319e5a2
Miles Mosweu <tmtmosweu@gmail.com>
2023-07-12 10:42:44 +0200

    Add set_post callback function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,13 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
+  
   def index
     @posts = Post.all
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
