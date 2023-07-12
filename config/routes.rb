f7c0939011aab56e71c230d84c5bc0f6226431a0
Miles Mosweu <tmtmosweu@gmail.com>
2023-07-12 11:31:22 +0200

    Add posts routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   
   # root "articles#index"
 
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 end
