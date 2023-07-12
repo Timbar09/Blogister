17975741b88bdd64dbd8cacfc6e4e8b0e6f3aa2f
Miles Mosweu <tmtmosweu@gmail.com>
2023-07-12 10:14:01 +0200

    Add set_user callback function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+  
   def index
     @users = User.all
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
