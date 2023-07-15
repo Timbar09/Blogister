ec457c932e4c24982ef8281e7a6296b7b6e01c49
Miles Mosweu <tmtmosweu@gmail.com>
2023-07-15 19:45:13 +0200

    Add posts#index requests specs

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'Posts', type: :request do
+  describe 'GET' do
+    before :all do
+      @taker = User.create(name: 'The Undertaker', photo: 'https://picsum.photos/200/300', bio: 'The Phenom')
+    end
+
+    describe '/posts#index' do
+      before :each do
+        get user_posts_path(@taker)
+      end
+
+      it 'should return a successful response' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'should render the index template' do
+        expect(response).to render_template(:index)
+      end
+
+      it 'should include the placeholder text' do
+        expect(response.body).to include('A list of posts by a given user')
+      end
+    end
+  end
+end
