7da8557c34c031d5accd2fa98e642b0dded9e1ab
Miles Mosweu <tmtmosweu@gmail.com>
2023-07-17 12:21:25 +0200

    Add new and create post methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,20 @@ class PostsController < ApplicationController
 
   def show; end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.create(post_params)
+
+    if @post.save
+      redirect_to user_post_path(current_user, @post), notice: 'Post was successfully created.'
+    else
+      render :new, alert: 'Post was not created.'
+    end
+  end
+
   private
 
   def set_user
