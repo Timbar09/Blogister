d16904884c3dd311c5a0019cc7f9ab31b662e83a
Miles Mosweu <tmtmosweu@gmail.com>
2023-07-17 19:50:57 +0200

    Add new and create methods methods

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,20 @@
 class CommentsController < ApplicationController
   before_action :set_post, only: [:create]
 
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = @post.comments.new(comment_params.merge(author: current_user))
+
+    if @comment.save
+      redirect_to post_path(@post), notice: 'Comment was successfully created.'
+    else
+      render :new, alert: 'Comment was not created.'
+    end
+  end
+
   private
 
   def set_post
