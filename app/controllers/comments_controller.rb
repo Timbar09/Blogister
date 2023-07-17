e4004f0c3d0c188c9f1ee6d5e377f607ec75d5a2
Miles Mosweu <tmtmosweu@gmail.com>
2023-07-17 19:47:45 +0200

    Add comments controller private methods

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
+  before_action :set_post, only: [:create]
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
