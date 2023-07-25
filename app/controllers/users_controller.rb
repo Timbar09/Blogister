261ef5c733006d41c99580d4e728a584abe8fce2
eudondian <eudondian@gmail.com>
2023-07-25 11:02:43 +0100

    Fix n+1 problem on users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show]
 
   def index
-    @users = User.all
+    @users = User.include(post).all
   end
 
   def show; end
