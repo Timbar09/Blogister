1110d82cc6254a0e078d25728af6921e93c25583
Miles Mosweu <tmtmosweu@gmail.com>
2023-07-29 17:36:02 +0200

    Add API controllers and routes

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,11 @@
+module Api
+  module V1
+    class PostsController < ApplicationController
+      def index
+        @user = User.find(params[:user_id])
+        @posts = @user.posts
+        render json: @posts, status: :ok
+      end
+    end
+  end
+end
