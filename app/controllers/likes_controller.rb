dfe30010d9cdab0b355ef87fe1dc43cdf8727ce5
Miles Mosweu <tmtmosweu@gmail.com>
2023-08-11 15:40:00 +0200

    Update likes views and controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,15 +6,18 @@ class LikesController < ApplicationController
     @like = Like.find_by(author: current_user, post: @post)
 
     if @like
-      @like.destroy
-      redirect_back(fallback_location: user_post_path(@post.author, @post), notice: 'Post was successfully unliked.')
+      if @like.destroy
+      redirect_back(fallback_location: user_post_path(@post.author, @post), notice: 'You successfully unliked the post.')
+      else
+        redirect_back(fallback_location: user_post_path(@post.author, @post), alert: 'Error: Could not unlike the post.')
+      end
     else
       @like = Like.new(author: current_user, post: @post)
 
       if @like.save
-        redirect_back(fallback_location: user_post_path(@post.author, @post), notice: 'Post was successfully liked.')
+        redirect_back(fallback_location: user_post_path(@post.author, @post), notice: 'You successfully liked the post.')
       else
-        redirect_back(fallback_location: user_post_path(@post.author, @post), alert: 'Error: Post was not liked.')
+        redirect_back(fallback_location: user_post_path(@post.author, @post), alert: 'Error: Could not like the post.')
       end
     end
   end
