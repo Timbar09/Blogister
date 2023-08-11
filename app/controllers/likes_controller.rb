5b35e5110ee365a276c4fa99688392d67d8ef455
Miles Mosweu <tmtmosweu@gmail.com>
2023-08-11 16:17:09 +0200

    Update nav and resolve linters

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -7,15 +7,22 @@ class LikesController < ApplicationController
 
     if @like
       if @like.destroy
-      redirect_back(fallback_location: user_post_path(@post.author, @post), notice: 'You successfully unliked the post.')
+        redirect_back(
+          fallback_location: user_post_path(@post.author, @post),
+          notice: 'You successfully unliked the post.'
+        )
       else
-        redirect_back(fallback_location: user_post_path(@post.author, @post), alert: 'Error: Could not unlike the post.')
+        redirect_back(
+          fallback_location: user_post_path(@post.author, @post),
+          alert: 'Error: Could not unlike the post.'
+        )
       end
     else
       @like = Like.new(author: current_user, post: @post)
 
       if @like.save
-        redirect_back(fallback_location: user_post_path(@post.author, @post), notice: 'You successfully liked the post.')
+        redirect_back(fallback_location: user_post_path(@post.author, @post),
+                      notice: 'You successfully liked the post.')
       else
         redirect_back(fallback_location: user_post_path(@post.author, @post), alert: 'Error: Could not like the post.')
       end
